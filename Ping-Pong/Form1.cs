3157fdbd7762d36f61855465da0b93fb64ca770f
OlSen99 <Olgary58@mail.ru>
2020-05-27 01:20:29 +0300

    Panel-computer

--- Ping-Pong/Form1.cs
+++ Ping-Pong/Form1.cs
@@ -12,13 +12,46 @@ namespace Ping_Pong
 {
     public partial class pingPong : Form
     {
-        public pingPong()
+        bool? flag = true; // Добавочная переменная для определения условия
+        int rightForm = 670;// Предел высоты формы
+
+        public pingPong()// Инициализация объектов
         {
             InitializeComponent();
         }
 
-        private void timerTick(object sender, EventArgs e)
+        private void timerTick(object sender, EventArgs e)// Отображает изменение формы каждую секудну
         {
+            computer(pictureLeft);
+        }
+
+        private void computer(PictureBox picture)//Панель, которой управляет компьютер
+        {
+
+            if (picture.Location.Y < rightForm - picture.Height && picture.Location.Y > 0 && flag == false)
+            {
+                picture.Location = new Point(picture.Location.X,
+                                        Math.Max(0, Math.Min(rightForm - picture.Height, picture.Location.Y - 4)));
+            }
+
+            else if (picture.Location.Y >= rightForm - picture.Height)
+            {
+                picture.Location = new Point(picture.Location.X,
+                Math.Max(0, Math.Min(rightForm - picture.Height, picture.Location.Y - 4)));
+                flag = false;
+
+            }
+            else if (picture.Location.Y <= 0)
+            {
+                picture.Location = new Point(picture.Location.X,
+                                        Math.Max(0, Math.Min(rightForm - picture.Height, picture.Location.Y + 4)));
+                flag = true;
+            }
+
+            else if (picture.Location.Y < rightForm - picture.Height && flag == true)
+                picture.Location = new Point(picture.Location.X,
+                                        Math.Max(0, Math.Min(rightForm - picture.Height, picture.Location.Y + 4)));
+
 
         }
     }
